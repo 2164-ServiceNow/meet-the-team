cfce55efb1f50d3c4ab628988f4e82edbb750008
venubabuperam <venubabu119@gmail.com>
2025-01-09 14:11:45 +0530

    commit

---
+++ venu.js
@@ -0,0 +1,19 @@
+if(true){
+  var v="venu";
+}
+console.log(v);
+
+let b;
+console.log(b)
+b="babu";
+console.log(b);
+
+
+function add(x,y){
+    m=3;
+  return x+y;
+}
+
+console.log( add(10,24));
+
+console.log(m);
