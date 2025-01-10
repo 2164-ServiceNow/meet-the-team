9e90bacbe585304b9634e96d27dc92f9a239abb5
SumaGuruvu <sumaguruvu@gmail.com>
2025-01-10 16:43:34 +0530

    added traditional css styling

--- introduction-sumaguruvu.js
+++ introduction-sumaguruvu.js
@@ -1,5 +1,46 @@
-let submitButton=document.getElementById("submit-button")
 function changeText()
 {
-    alert("yay! thanks for the review.");
+    alert("oopsie!long way to go.");
+    document.getElementById("catchme").textContent=`caughtme!`;
 }
+const catchMe=document.getElementById('catchme');
+catchMe.addEventListener('focus',()=>{
+    catchMe.style.borderColor='darkgreen';
+})
+catchMe.addEventListener('blur',()=>{
+    catchMe.style.borderColor='voilet';
+})
+const messageElement=document.getElementById("dynamic-message");
+const currentTime=new Date();
+messageElement.textContent=`Welcome! The current time is: ${currentTime.toLocaleTimeString()}`;
+
+const form=document.getElementById("form");
+const emailInput=document.getElementById("emailInput");
+const emailError=document.getElementById("emailerror");
+
+form.addEventListener('submit',(event)=>{
+    if(!emailInput.checkValidity())
+    {
+        event.preventDefault();
+        emailError.style.display='block';
+    }
+    else{
+        emailError.style.display='none';
+    }
+})
+
+const image1=document.getElementById("image1");
+image1.addEventListener('mouseover',()=>{
+    image1.src="./Images/sw3.jpg";
+});
+image1.addEventListener('mouseout',()=>{
+    image1.src="./Images/sw2.jpg";
+})
+
+const image2=document.getElementById("image2");
+image2.addEventListener('mouseover',()=>{
+    image2.src="./Images/sw1.webp";
+});
+image2.addEventListener('mouseout',()=>{
+    image2.src="./Images/sw3.webp";
+})
